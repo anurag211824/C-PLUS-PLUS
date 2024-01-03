5aba1444ea1dc5d8d8b6c875a6f952988592b9c2
anurag211824 <138526681+anurag211824@users.noreply.github.com>
2024-01-04 00:05:51 +0530

    some modification in frog  jump

--- L-27_RECURSION/13_frog_jump.cpp
+++ L-27_RECURSION/13_frog_jump.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int frog_jump_recursive(int *arr,int n,int index){
     //base case
-    if(index>=n-1)return 0;
+    if(index==n-1)return 0;
+    if(index==n-2)return abs(arr[n-1]-arr[n-2]);
     
     
-    int sum1=frog_jump_recursive(arr,n,index+1)+abs(arr[index+1]-arr[index]);
-    int sum2=frog_jump_recursive(arr,n,index+2)+abs(arr[index+2]-arr[index]);
+    int sum1=abs(arr[index+1]-arr[index])+frog_jump_recursive(arr,n,index+1);
+    int sum2=abs(arr[index+2]-arr[index])+frog_jump_recursive(arr,n,index+2);
     return min(sum1,sum2);
 }
 
