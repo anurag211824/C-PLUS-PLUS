40727739efc63689490b0de4d9373182b8fa61fb
anurag211824 <138526681+anurag211824@users.noreply.github.com>
2024-01-05 21:29:01 +0530

    grid problem understood

--- L-27_RECURSION/14_grid_path.cpp
+++ L-27_RECURSION/14_grid_path.cpp
@@ -1,16 +1,20 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
-int path(int i,int j,int m,int n){
-    if(i==m-1)return 1;
-    if(j==n-1)return 1;
+int path(int i, int j, int m, int n)
+{
+    if (i == m - 1)
+        return 1;
+    if (j == n - 1)
+        return 1;
 
-    return path(i+1,j,m,n)+path(i,j+1,m,n);
+    return path(i + 1, j, m, n) + path(i, j + 1, m, n);
 }
 
-int main(){
-   
-    int res=path(0,0,3,3);
-    cout<<res;
-return 0;
+int main()
+{
+
+    int res = path(0, 0, 3, 3);
+    cout << res;
+    return 0;
 }
---
+++ L-29_RECUSION_ON_STRING/3_printing_the_subsquence_of_string.cpp
@@ -0,0 +1,35 @@
+/*
+input abc
+output a,b,c,ab,bc,ac,abc,NUll
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+void print_subsquence(string &str, int n, int i, vector<string> &res, string str1)
+{
+    if (i == n)
+    {
+        res.push_back(str1);
+        return;
+    }
+    print_subsquence(str, n, i + 1, res, str1 += str[i]);
+    str1.pop_back();
+      
+    print_subsquence(str, n, i + 1, res, str1);
+}
+
+int main()
+{
+    string str = "abc";
+    int n = str.size();
+    string str1;
+    vector<string> res;
+    print_subsquence(str,n,0,res,str1);
+    for (int i = 0; i < res.size(); i++)
+    {
+        cout << res[i] << " ";
+    }
+
+    return 0;
+}
