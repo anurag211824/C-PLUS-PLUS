75868ee73b36128f7e2062bdc8402e647661c90a
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2024-05-01 18:30:14 +0530

    change function

--- L-15_TWO_POINTER_APPROACH/1.cpp
+++ L-15_TWO_POINTER_APPROACH/1.cpp
@@ -10,7 +10,6 @@ void sort0and1array(int arr[], int size)
         if (arr[i] == 0)
             count_0++;
     }
-
     for (int i = 0; i < size; i++)
     {
         if (i < count_0)
--- L-30_STRINGS/7_find_substring.cpp
+++ L-30_STRINGS/7_find_substring.cpp
@@ -6,15 +6,16 @@ void substring_genertaor(string str, vector<string> &substrings)
     int substr_size = n;
     for (int i = 0; i < n; i++)
     {
-        for (int j = i+1; j<=n; j++)
+        for (int j = i+1; j<=substr_size; j++)
         {
-            substrings.push_back(str.substr(i,j-i));
+            substrings.push_back(str.substr(i,j));
         }
+         substr_size--;
     }
 }
 int main()
 {
-    string str = "abc";
+    string str = "abcd";
     vector<string> substrings;
     substring_genertaor(str, substrings);
     for (int i = 0; i < substrings.size(); i++)
